2a5aad5d3904b73e94dee4d476222078a1c19761
Mark Tripoli <mgtripoli@comcast.net>
2022-08-12 13:40:15 -0400

    do some restructure

--- src/app/app.functions/generate-meme.js
+++ src/app/app.functions/generate-meme.js
@@ -14,24 +14,49 @@ exports.main = async (context = {}, sendResponse) => {
     });
 
     try {
-        const memes = await axios.get('https://api.imgflip.com/get_memes');
+        const memes = await axios.get('https://meme-api.herokuapp.com/gimme/1');
         // memes contains a list of 100 memes, get a random one
-        const randomMeme = memes.data.data.memes[Math.floor(Math.random() * memes.data.data.memes.length)];
+        const randomMeme = memes.data.memes[0];
 
-        sendResponse({
-            sections: [
-                {
-                    type: 'text',
-                    text: `Hi ${firstname}, here's your meme:`,
-                },
-                {
-                    type: 'image',
-                    imageUrl: randomMeme.url,
-                    altText: randomMeme.name,
-                },
-            ],
-        });
+        const memeSections = [
+            {
+                type: 'text',
+                body: [
+                    {
+                        type: 'text',
+                        text: `Hi ${firstname}, here's your meme titled: `${randomMeme.title},
+                    },
+                    {
+                        type: 'text',
+                        text: `This was originally posted on the subreddit ${randomMeme.subreddit} at: ${randomMeme.postLink}`,
+                    },
+                    {
+                        type: 'image',
+                        imageUrl: randomMeme.url,
+                        altText: randomMeme.name,
+                    },
+                    {
+                        type: 'image',
+                        imageUrl: randomMeme.url,
+                        altText: randomMeme.name,
+                    },
+                ]
+            },
+            {
+                type: 'text',
+                text: 'Press this button to get a Dank Meme!',
+            },
+            {
+                type: 'button',
+                text: 'Meme Me',
+                onClick: {
+                    type: 'SERVERLESS_ACTION_HOOK',
+                    serverlessFunction: 'generate-meme',
+                }
+            }
+        ];
 
+        sendResponse({sections: [...memeSections]});
     } catch (error) {
         sendResponse({
             message: {
