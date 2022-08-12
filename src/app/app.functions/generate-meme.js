bdb87ff9ed954e22ed47172c965c5eae362824f2
Mark Tripoli <mgtripoli@comcast.net>
2022-08-12 14:15:46 -0400

    hmm

--- src/app/app.functions/generate-meme.js
+++ src/app/app.functions/generate-meme.js
@@ -49,7 +49,45 @@ exports.main = async (context = {}, sendResponse) => {
             }
         ];
 
-        sendResponse({sections: [...memeSections]});
+        sendResponse({
+            sections: [
+                {
+                    type: 'tile',
+                    body: [
+                        {
+                            type: 'text',
+                            text: `Hi ${firstname}, here's your meme titled: `${randomMeme.title},
+                        },
+                        {
+                            type: 'text',
+                            text: `This was originally posted on the subreddit ${randomMeme.subreddit} at: ${randomMeme.postLink}`,
+                        },
+                        {
+                            type: 'image',
+                            imageUrl: randomMeme.url,
+                            altText: randomMeme.name,
+                        }
+                    ]
+                },
+                {
+                    type: 'tile',
+                    body: [
+                        {
+                            type: 'text',
+                            text: 'Press this button to get a Dank Meme!',
+                        },
+                        {
+                            type: 'button',
+                            text: 'Meme Me',
+                            onClick: {
+                                type: 'SERVERLESS_ACTION_HOOK',
+                                serverlessFunction: 'generate-meme',
+                            }
+                        }
+                    ]
+                }
+            ]
+        });
     } catch (error) {
         sendResponse({
             message: {
